8e5527d26917c46545ae5454545184a3b1a893a2
Shreerag Marar <shreerag.krishnakumar.marar@smu.ca>
2022-10-26 01:46:53 -0300

    implemented temp feature

--- src/MyTown.js
+++ src/MyTown.js
@@ -1,6 +1,21 @@
 import pune from "./Pune.png";
+import cold from "./assets/cold.png";
+import mild from "./assets/mild.png";
+import sunny from "./assets/sunny.png";
+import { useEffect, useState } from "react";
 
 const MyTown = (props) => {
+  //   let tmp = 0;/z
+  const [tmp, setTemp] = useState(0);
+  async function fetchTemperature() {
+    let temp = await getWeather();
+    temp = temp.temperature - 273.15;
+    //console.log(temp);
+    setTemp(temp);
+  }
+  useEffect(() => {
+    fetchTemperature();
+  }, []);
   return (
     <div>
       <img src={pune} className="App-logo"></img>
@@ -13,15 +28,29 @@ const MyTown = (props) => {
         of Maharashtra state. It has been ranked "the most liveable city in
         India" several times
       </p>
-      {getHighMsg()}
+      {console.log(tmp)}
+      {tmp < 10 ? (
+        <img src={cold}></img>
+      ) : tmp > 10 && tmp < 20 ? (
+        <img src={mild}></img>
+      ) : (
+        <img src={sunny}></img>
+      )}
+      }
     </div>
   );
   async function getWeather() {
     try {
-      const response = await fetch(
-        "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
-      );
-      return await response.json();
+      let response = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?lat=18.5204&lon=73.8567&appid=51031104c083849ac96df17e30cdb8e2"
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          return data["main"]["temp"];
+        });
+      return {
+        temperature: response,
+      };
     } catch (error) {
       return [];
     }
